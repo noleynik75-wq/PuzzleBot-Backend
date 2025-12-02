c77edb0572f89f06659e2c08247b6b95cf470e40
noleynik75-wq <n.oleynik75@gmail.com>
2025-12-02 22:32:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
-import os
 from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
 from openai import OpenAI
+import os
 
 app = FastAPI()
 
-# читаем API KEY из переменной окружения
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 @app.get("/")
 def root():
     return {"status": "ok", "message": "Backend is running!"}
 
+
 @app.post("/webhook")
 async def webhook(request: Request):
     data = await request.json()
-
-    # PuzzleBot формирует сообщение в поле "message"
     user_message = data.get("message", "")
 
     if not user_message:
-        return JSONResponse({"reply": "Я не получила текст сообщения."})
+        return {"reply": "Сообщение пустое"}
 
-    # OpenAI запрос
+    # Запрос к OpenAI
     response = client.responses.create(
         model="gpt-4o-mini",
         input=user_message
@@ -30,4 +27,6 @@ async def webhook(request: Request):
 
     bot_reply = response.output_text
 
-    return JSONResponse({"reply": bot_reply})
+    return {
+        "reply": bot_reply
+    }
